5683a2e596b5ef74f8667a64c0342972dcb015ee
AriannaMadrid <arianna@decima.us>
2025-05-27 00:30:35 -0400

    endpoint de backend actualizado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  apiUrl: 'https://task-app-api-9hcg.onrender.com'
   // apiUrl: 'http://127.0.0.1:5001/task-app-293dd/us-central1/api'
-  apiUrl: 'http://localhost:3000'
+  // apiUrl: 'http://localhost:3000'
 }
